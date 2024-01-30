8e575d2f7b73de2ea4de6e6e13092c1a441829d9
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-01-30 15:53:28 +0100

    This is going to be my first but not last commit

--- S01/sum20.py
+++ S01/sum20.py
@@ -1,6 +1,9 @@
-count = 0
+res = 0
 for i in range(1, 21):
-    count += i
-print(count)
+    res += i
+print("Total sum: ", res)
 
-
+l = []
+for i in range(1, 21):
+    l.append(i)
+print("L = ", l)
