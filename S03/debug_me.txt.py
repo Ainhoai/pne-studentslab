cf7865191a99d8bfe36b9ab0578640054792aedc
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-01-31 17:30:59 +0100

    This is going to be my first but not last commit

--- S03/debug_me.txt.py
+++ S03/debug_me.txt.py
@@ -1,1 +1,3 @@
-#The error is found in t3, the 8th line, because it is not possible to divide by t1 because t1 is equal to 0. This error is due to the fact that in line 6, where g has the possibility to be zero, therefor when t1 = g(c, d) the result can be zero. It is impossible to divide by zero.
+#The error is found in t3, the 8th line, because it is not possible to divide by t1 because t1 is equal to 0.
+#This error is due to the fact that in line 6, where g has the possibility to be zero, therefor when t1 = g(c, d) the result can be zero.
+#It is impossible to divide by zero.
