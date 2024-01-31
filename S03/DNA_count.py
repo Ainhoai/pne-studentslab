e902ae5db2497f050c46905a106c35c65f5785c2
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-01-31 18:09:25 +0100

    This is going to be my first but not last commit

--- S03/DNA_count.py
+++ S03/DNA_count.py
@@ -1,23 +1,17 @@
-c_a = 0
-c_c = 0
-c_g = 0
-c_t = 0
+
 dna_sequence = input("Please, enter a dna sequence: ")
+letters = {"A":  0, "C": 0, "G": 0, "T": 0}
 for c in dna_sequence:
     if c == "A":
-        c_a += 1
+        letters["A"] += 1
     elif c == "C":
-        c_c += 1
+        letters["C"] += 1
     elif c == "G":
-        c_g += 1
+        letters["G"] += 1
     elif c == "T":
-        c_t += 1
+        letters["T"] += 1
 
-total_count = c_a + c_c + c_g + c_t
+total_count = len(dna_sequence)
 print(total_count)
-print("A:", c_a)
-print("C:", c_c)
-print("G:", c_g)
-print("T:", c_t)
-
-letters = {1: "A", 2: "C", 3: "G", 4: "T"}
+for x, y in letters.items():
+    print("{}: {}".format(x, y))
