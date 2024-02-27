f26577dd2ff54faddbb63d63023d42d483be2b4d
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-02-27 10:32:38 +0100

    commit message

--- S06/Seq-01.py
+++ S06/Seq-01.py
@@ -19,17 +19,26 @@ class Gene(Seq):
     def __init__(self, strbases, name= ""):
         super().__init__(strbases)
         self.name = name
-        print("New Gene created!")
+        print("New Gene created")
+
+
+    def __str__(self):
+        return self.name + "-" + self.strbases
+
 
 
 
 s1 = Seq("AGTACACTGGT")
 s2 = Seq("CGTAAC")
+g = Gene("CGTAAC", "FRAT1")
+
 
 print(f"Sequence 1: {s1}")
 print(f"  Length: {s1.len()}")
 print(f"Sequence 2: {s2}")
 print(f"  Length: {s2.len()}")
+print(f"Gene: {g}")
+
 
 
 
