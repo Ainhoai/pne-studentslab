870de7da2440eb3bb68dca8436f0fe147bc22e28
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-02-27 16:29:34 +0100

    Push and commit

--- P01/e9.py
+++ P01/e9.py
@@ -1,17 +1,15 @@
 from Seq1 import Seq
-
 PRACTICE = 1
 EXERCISE = 9
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 
-
 s = Seq()
 FILENAME = ["U5.txt"]
-print("Null sequence created.")
-for i, s in enumerate(FILENAME):
-    print(f"Sequence {i + 1}: (length: {s.len()}) {s}")
-    print(f"\tBases: {s.count()}")
-    print(f"\tRev: {s.reverse()}")
-    print(f"\tComp: {s.complement()}")
-s.read_fasta(FILENAME)
+
+if s == "NULL":
+    print("Null sequence created.")
+else:
+    pass
+
+
