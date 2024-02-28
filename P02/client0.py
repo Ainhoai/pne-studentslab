77f21f29dcf0b38486c56e701d7d61a7ccd17e47
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-02-28 17:56:43 +0100

    Push and commit

--- P02/client0.py
+++ P02/client0.py
@@ -1,3 +1,4 @@
+import socket
 class Client:
     def __init__(self, IP, PORT):
         self.IP = IP
@@ -8,10 +9,26 @@ class Client:
         print("Ok!")
 
 
-    def __str__(self):
+    def __str__(self, msg):
 
         return f"Connection to SERVER at {self.IP}, PORT: {self.PORT}"
 
-    def talk(self, socket, msg):
-        pass
+    def talk(self, msg):
+        # -- Create the socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # establish the connection to the Server (IP, PORT)
+        s.connect((self.IP, self.PORT))
+
+        # Send data.
+        s.send(str.encode(msg))
+
+        # Receive data
+        response = s.recv(2048).decode("utf-8")
+
+        # Close the socket
+        s.close()
+
+        # Return the response
+        return response
 
