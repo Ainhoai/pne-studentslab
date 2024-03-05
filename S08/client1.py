d2beeae61d43cf26e194d662757d5aef66105e15
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-03-05 11:40:42 +0100

    commit message

--- S08/client1.py
+++ S08/client1.py
@@ -3,13 +3,11 @@ import socket
 SERVER_PORT = 8081
 SERVER_IP = ""
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect((SERVER_IP, SERVER_PORT))
+clientsocket.connect((SERVER_IP, SERVER_PORT))
 
-# Send data. No strings can be sent, only bytes
-# It necessary to encode the string into bytes
-s.send(str.encode("HELLO FROM THE CLIENT!!!"))
+clientsocket.send(str.encode("HELLO FROM THE CLIENT!!!"))
 
 # Close the socket
-s.close()
+clientsocket.close()
