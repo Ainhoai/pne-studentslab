60ff85020809bb56ffe59596669fdd51aec9df37
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-03-06 13:29:30 +0100

    commit message

--- P02/e5.py
+++ P02/e5.py
@@ -4,9 +4,21 @@ import os
 
 PRACTICE = 2
 EXERCISE = 5
-Genes = ["ADA.txt", "U5.txt", "FRAT1.txt"]
+Gene = "FRAT1.txt"
+NUMBER_OF_FRAGMENTS = 5
+NUMBER_OF_BASES = 10
 
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
-IP = "192.168.0.33" #servidor laptop dulce
+IP = "192.168.0.30" #servidor laptop prof
 PORT = 8081
+
+c = Client(IP, PORT)
+
+filename = os.path.join("..", "sequences", Gene)
+s = Seq()
+s.read_fasta(filename)
+print(f"Gene {Gene},  {s}")
+
+for g in range(1, NUMBER_OF_FRAGMENTS + 1):
+    print(f"Fragment: ")
