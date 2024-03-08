1d000dd26ef23ae963dc549292251a58b2388550
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-03-08 12:15:42 +0100

    commit message

---
+++ P03/Seq1.py
@@ -0,0 +1,97 @@
+from pathlib import Path
+
+
+class Seq:
+    """ A class to represent  sequences """
+    BASES = ["A", "G", "C", "T"]
+
+    def __init__(self, strbases= None):
+        if strbases is None or len(strbases) == 0:
+            self.strbases = "NULL"
+            print("NULL sequence created.")
+        else:
+            ok = True
+            for b in strbases:
+                if b not in Seq.BASES:
+                    ok = False
+                    self.strbases = "ERROR!"
+                    print("Incorrect sequence detected.")
+                    break
+            if ok:
+                self.strbases = strbases
+                print("A new sequence is created!")
+
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        if self.strbases is "NULL" or self.strbases is "ERROR!":
+            return 0
+        return len(self.strbases)
+
+    def count_bases(self, base):
+        if self.strbases is "NULL" or self.strbases is "ERROR!":
+            return 0
+        return self.strbases.count(base)
+
+    def count(self):
+        base_appearances = {}
+        for base in Seq.BASES:
+            base_appearances[base] = self.count_bases(base)
+        return base_appearances
+
+    def reverse(self):
+        if self.strbases is "NULL":
+            return "NULL"
+
+        elif self.strbases is "ERROR!":
+            return "ERROR!"
+        else:
+            new_seq = self.strbases[:]
+            return new_seq[::-1]
+
+    def complement(self):
+        if self.strbases is "NULL":
+            return "NULL"
+
+        elif self.strbases is "ERROR!":
+            return "ERROR!"
+        else:
+            bases_complement = {"A": "T", "T": "A", "C": "G", "G": "C"}
+            comp = ""
+            for base in self.strbases:
+                comp += bases_complement[base]
+            return comp
+
+    def read_fasta(self, filename):
+        file_content = Path(filename).read_text()
+        lines = file_content.splitlines()
+        body = lines[1:]
+
+        dna_sequence = ""
+        for line in body:
+            dna_sequence += line
+        self.strbases = dna_sequence
+
+    def max_base(self):
+        bases_dict = {}
+        for b in Seq.BASES:
+            bases_dict[b] = self.count_bases(b)
+        most_frequent_base = max(bases_dict, key=bases_dict.get)
+
+        return most_frequent_base
+
+class Gene(Seq):
+    """This class is derived from the Seq Class
+           All the objects of class Gene will inherit
+           the methods from the Seq class
+        """
+    def __init__(self, strbases, name=""):
+        super().__init__(strbases)
+        self.name = name
+        print("New Gene created")
+
+
+
+    def __str__(self):
+        return self.name + "-" + self.strbases
--- P03/seq_server.py
+++ P03/seq_server.py
@@ -10,18 +10,25 @@ try:
     server_socket.bind((IP, PORT))
     server_socket.listen()
 
+    print("SEQ Server configure.")
+
     while True:
-        print(f"Waiting for connections at ({IP}:{PORT})...")
+        print(f"Waiting for clients ({IP}:{PORT})...")
         (client_socket, client_address) = server_socket.accept()
 
         request_bytes = client_socket.recv(2048)
-        request = request_bytes.decode("utf-8") #peticion que me llega del cliente.
-        print("Message from client: ", end="")
-        termcolor.cprint(request, 'green')
+        request = request_bytes.decode("utf-8")
 
-        response = f"ECHO: {request}\n"
-        response_bytes = str.encode(response)
+
+        slices = request.split(" ")
+        command = slices[0]
+        if command == "PING":
+            response = "Ok!\n"
+        print(response)
+        response_bytes = response.encode()
         client_socket.send(response_bytes)
+
+
 
         client_socket.close()
 
