6a0047ae177d7a4945ab33bb13450a4f9be85989
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-03-12 12:10:45 +0100

    commit message

--- P03/test-client.py
+++ P03/test-client.py
@@ -1,18 +1,52 @@
 from client0 import Client
+PRACTICE = 3
+EXERCISE = 7
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8080
+N = 5
+SEQUENCE = "ACCTCCTCTCCAGCAATGCCAACCCCAGTCCAGGCCCCCATCCGCCCAGGATCTCGATCA"
+GENES = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
 
 c = Client(SERVER_IP, SERVER_PORT)
+print(f"* Testing PING...")
 response = c.talk("PING")
 print(response)
-response = c.talk("GET 2")
+
+print(f"* Testing GET...")
+for n in range(N): #N: constante creada previamente; n: index.
+    response = c.talk(f"GET {n}")
+    print(f"GET: {response}")
+
+print(f"* Testing INFO...")
+print(f"INFO {SEQUENCE}")
+response = c.talk(f"INFO {SEQUENCE}")
 print(response)
-response = c.talk("INFO AACCGTA")
+
+print(f"* Testing COMP...")
+print(f"COMP {SEQUENCE}")
+response = c.talk(f"COMP {SEQUENCE}")
 print(response)
-response = c.talk("COMP AACCGTA")
+
+print(f"* Testing REV...")
+print(f"REV {SEQUENCE}")
+response = c.talk(f"REV {SEQUENCE}")
 print(response)
-print(c.talk("REV AACCGTA"))
-print(response)
-response = c.talk("GENE U5")
-print(response)
+
+print(f"* Testing GENE...")
+for g in GENES:
+    print(f"GENE {GENES}")
+    response = c.talk(f"GENE {GENES}")
+    print(response)
+
+
+
+
+
+
+
+
+
