2330476247de3c06228ba66b335e7b39b66f6bc0
ainhoaib <a.ibanez.2023@alumnos.urjc.es>
2024-03-13 11:05:14 +0100

    commit message

--- GAME/game.py
+++ GAME/game.py
@@ -1,46 +1,69 @@
-from pathlib import Path
+
 import socket
 import random
 class Numberguesser:
 
-    ATTEMPTS = 0
-    player_number = int(input(f"Please enter a valid number between 0-100:"))
-    secret_number = random.randint(1, 100)
+    MIN = 1
+    MAX = 100
 
-    def __init__(self, secret_number, attempts):
-        self.secret_number = secret_number
-        self.attempts = attempts
+    def __init__(self):
+        self.secret_number = random.randint(Numberguesser.MIN, Numberguesser.MAX)
+        self.attempts = []
+        self.guessed = False
 
-        server_port = 8081
-        server_ip = "127.0.0.1"
+    def __str__(self):
+        return f"Secret number: {self.secret_number} - Attempt: {self.attempts}"
 
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        try:
-            server_socket.bind((server_ip, server_port))
-            server_socket.listen()
-
-            while True:
-                print(f"Waiting for connections at {server_ip}, {server_port} ")
-                (client_socket, address) = server_socket.accept()
-                msg = client_socket.recv(2048).decode("utf-8")
-                print(f"Message from client: {msg}")
-                message = "Hello from the teacher's server\n"
-                send_bytes = str.encode(message)
-                client_socket.send(send_bytes)
-                client_socket.close()
-        except socket.error:
-            print(f"Problems using ip {server_ip} port {server_port}. Is the IP correct? Do you have port permission?")
-        except KeyboardInterrupt:
-            print("Server stopped by the user")
-            server_socket.close()
-
-    def guesser(self):
-        if 0 <= self.player_number <= 100:
-            Numberguesser.ATTEMPTS += 1
-            print(f"You won after {Numberguesser.ATTEMPTS} attempts")
-        elif self.player_number > 100:
-            Numberguesser.ATTEMPTS += 1
-            print(f"Lower")
-        elif self.player_number < 100:
-            Numberguesser.ATTEMPTS += 1
+    def guess(self, number):
+        self.attempts.append(number)
+        if number < self.secret_number:
             print(f"Higher")
+        elif number > self.secret_number:
+            print(f"Lower")
+        else:
+            self.guessed = True
+            return f"You won after {len(self.attempts)} attempts."
+        return "Well guessed"
+
+    def is_guessed(self):
+        return self.guessed
+
+server_port = 8081
+server_ip = "127.0.0.1"
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+try:
+    server_socket.bind((server_ip, server_port))
+    server_socket.listen()
+
+    print("Guess the number server configured!")
+
+    while True:
+        print(f"Waiting for connections at {server_ip}, {server_port}... ")
+        (client_socket, address) = server_socket.accept()
+
+        nu = Numberguesser()
+        guessed = False
+        while not guessed:
+            request_bytes = client_socket.recv(2048)
+            request = request_bytes.decode("utf-8")
+            n = int(request)
+
+            response = nu.guess(n)
+            response_bytes = response.encode("utf-8")
+            client_socket.send(response_bytes)
+
+            print(nu)
+            if nu.is_guessed():
+                guessed = True
+        client_socket.close()
+
+except socket.error as e:
+    print(f"Communication error: {e}")
+except KeyboardInterrupt:
+    print("Server stopped by the user")
+    server_socket.close()
+
+
+
--- GAME/player.py
+++ GAME/player.py
@@ -1,25 +1,29 @@
 
 import socket
-class Player:
-    def __init__(self, ip, port):
-        self.ip = ip
-        self.port = port
-
-    def __str__(self):
-        return f"Connection to SERVER at {self.ip}, PORT: {self.port}"
-
-    def ping(self):
-        print("OK")
-
-    def talk(self, msg):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.ip, self.port))
-        s.send(str.encode(msg))
-        response = s.recv(2048).decode("utf-8")
-        s.close()
-        return response
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 8081
-c = Player(SERVER_IP, SERVER_PORT)
-player_number = int(input(f"Please enter a valid number between 0-100:"))
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((SERVER_IP, SERVER_PORT))
+
+guessed = False
+while not guessed:
+    try:
+        n = int(input("Please, enter a valid number:  "))
+        request = str(n)
+        request_bytes = request.encode("utf-8")
+        client_socket.send(request_bytes)
+
+        response_bytes = client_socket.recv(2048)
+        response = response_bytes.decode("utf-8")
+        if response == "Lower" or response == "Higher":
+            print(response)
+        else:
+            print(response)
+            guessed = True
+
+    except ValueError:
+        print("Please enter a valid number.")
+client_socket.close()
+
